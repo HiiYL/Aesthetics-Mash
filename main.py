010098066c93d99dec8b370e81f40c895e0054a6
HiiYL <yonglian146@gmail.com>
2016-11-27 18:15:20 +0800

    Added heatmap generation

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ import numpy as np
 
 import netifaces as ni
 
+
+from model import VGG_19_GAP_functional
+from utils import preprocess_image,deprocess_image
+import cv2
+
 UPLOAD_FOLDER = 'uploads/'
+HEATMAP_FOLDER = 'heatmaps/'
 TEST_IMAGES_FOLDER='test_images/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, static_url_path='')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['TEST_IMAGES_FOLDER'] = TEST_IMAGES_FOLDER
+app.config['HEATMAP_FOLDER'] = HEATMAP_FOLDER
+
+
+model = VGG_19_GAP_functional("aesthestic_gap_weights_1.h5", heatmap=True)
 
 def allowed_file(filename):
   return '.' in filename and \
@@ -35,20 +45,31 @@ def root():
 
     return render_template('index.html', comparison_set=[base, to_compare])
 
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect(url_for('root'))
+
 @app.route('/compare', methods=['POST'])
 def compare():
     values = request.values
     base = values['base']
     to_compare = values['compare']
 
-    to_compare_better = values['to_compare_better'] == 'true'
+    selected = int(values['selected'])
 
     base = ava_table.ix[int(base)]
     to_compare = ava_table.ix[int(to_compare)]
 
-    winner = (to_compare.score > base.score)
+    winner = np.argmax([base.score,to_compare.score])
 
-    correct = (winner == to_compare_better)
+    print(winner)
+    print(selected)
+
+    correct = (winner == selected)
+
+    print(correct)
 
     try:
         if correct:
@@ -68,12 +89,86 @@ def compare():
     #
     ########
 
-    return render_template('index.html', comparison_set=[base, to_compare], correct=correct)
+    # 937511
+    # 10017
 
+    base_image_path = 'test_images/{}.jpg'.format(base.name)
+    compare_image_path = 'test_images/{}.jpg'.format(to_compare.name)
+
+    # print(image_path)
+
+    heights = []
+    widths = []
+
+    base_img_original = cv2.imread(base_image_path)
+    width, height, _ = base_img_original.shape
+    base_im = preprocess_image(base_img_original)
+
+
+    widths.append(width)
+    heights.append(height)
+
+    compare_img_original = cv2.imread(compare_image_path)
+    width, height, _ = compare_img_original.shape
+    compare_im = preprocess_image(compare_img_original)
+
+    widths.append(width)
+    heights.append(height)
+
+    stacked_im = np.vstack([base_im, compare_im])
+
+    out = model.predict(stacked_im)
+
+    scores = out[0]
+    good_class_confidence = scores[:,1]
+    cpu_winner_predict = np.argmax(good_class_confidence)
+    cpu_correct = (winner == cpu_winner_predict)
+
+    try:
+        if cpu_correct:
+            session['cpu_current'] += 1
+    except:
+        session['cpu_current'] = 1 if cpu_correct else 0
+
+    conv_outputs = out[1]
+
+    for i in range(2):
+        conv_output = conv_outputs[i]
+        if i == 0:
+            process_and_generate_heatmap(conv_output, 
+                model, heights[i], widths[i], base_img_original, base_im, '{}.jpg'.format(base.name))
+        elif i == 1:
+            process_and_generate_heatmap(conv_output, 
+                model, heights[i], widths[i], compare_img_original, compare_im, '{}.jpg'.format(to_compare.name))
+
+    return render_template('index.html', comparison_set=[base, to_compare], correct=correct, cpu_confidence=good_class_confidence)
+
+
+
+def process_and_generate_heatmap(conv_output, model, height, width, img_original, img, img_name):
+    cam = np.zeros(dtype = np.float32, shape = conv_output.shape[1:3])
+    
+    class_weights = model.layers[-1].get_weights()[0]
+
+    class_to_visualize = 1 # 0 for bad, 1 for good
+    for j, w in enumerate(class_weights[:, class_to_visualize]):
+            cam += w * conv_output[j, :, :]
+    cam /= np.max(cam)
+    cam = cv2.resize(cam, (height, width))
+    heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
+    heatmap[np.where(cam < 0.2)] = 0
+    img = heatmap*0.5 + img_original
+    # filename = '{}.jpg'.format(base.name)
+    cv2.imwrite(os.path.join(app.config['HEATMAP_FOLDER'], img_name),img)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
   return send_from_directory(app.config['UPLOAD_FOLDER'],
+    filename)
+
+@app.route('/heatmaps/<filename>')
+def heatmap_file(filename):
+  return send_from_directory(app.config['HEATMAP_FOLDER'],
     filename)
 
 @app.route('/test_images/<filename>')
